3f63480f27caacdeafc08aabe6296dbf890073ca
GopinathRathinakali <gopinath.rathinakali@gmail.com>
2019-11-29 22:20:19 +0530

    Create Dictionaries_Assignment_2.py
    dictionary assignment

---
+++ Dictionaries_Assignment_2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 21:31:46 2019
+
+@author: God
+"""
+
+########### Practice 
+# Create the following 2 dictionaries
+states = {
+    'Oregon':'OR',
+    'Florida':'FL',
+    'California':'CA',
+    'New York': 'NY',
+    'Michigan':'MI'}
+
+cities = {
+    'CA':'San Fransico',
+    'MI': 'Detroit',
+    'NY':'Manhattan'}
+
+# what is the name of the city in state "MI"
+cities['MI']
+
+# Add a city 'Orlando' to 'FL'
+cities["FL"]='Orlando'
+
+# what is the city name in 'New York' State
+cities[states['New York']]
